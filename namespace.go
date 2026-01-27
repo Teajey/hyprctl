b2adc0a3136f1248df3e58f5cb66c0718dbd23ea
Teajey <21069848+Teajey@users.noreply.github.com>
2026-01-27 11:11:58 +0900

    Improve version-stamping code

--- namespace.go
+++ namespace.go
@@ -13,12 +13,37 @@ type Namespace struct {
 
 var docs xml.Comment
 
-func init() {
+const repo string = "github.com/Teajey/hyprctl"
+
+func getVersion() string {
 	version := "main"
-	if info, ok := debug.ReadBuildInfo(); ok {
-		version = info.Main.Version
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return version
 	}
-	docs = xml.Comment(fmt.Sprintf("See documentation for this version of hyprctl at https://github.com/Teajey/hyprctl/blob/%s/README.md", version))
+
+	// 1. Check if we are running tests/builds INSIDE the hyprctl repo
+	if info.Main.Path == repo {
+		if info.Main.Version != "" && info.Main.Version != "(devel)" {
+			return info.Main.Version
+		}
+	}
+
+	// 2. Check if we are a dependency of another project
+	for _, dep := range info.Deps {
+		if dep.Path == repo {
+			if dep.Version != "" && dep.Version != "(devel)" {
+				return dep.Version
+			}
+		}
+	}
+
+	return version
+}
+
+func init() {
+	version := getVersion()
+	docs = xml.Comment(fmt.Sprintf("See documentation for this version of hyprctl at https://%s/blob/%s/README.md ", repo, version))
 }
 
 func SetNamespace() Namespace {
